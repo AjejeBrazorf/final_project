76f63648e961831e1579d619ffc72e6246e31511
giarra1993 <dariogiarratana@libero.it>
2017-06-27 12:23:46 +0200

    Stampe di prova

--- src/main/java/com/javasampleapproach/security/SegnalationRestController.java
+++ src/main/java/com/javasampleapproach/security/SegnalationRestController.java
@@ -99,7 +99,9 @@ public class SegnalationRestController {
 			System.out.println("oldrate: " +oldRate);
 			if(oldRate != null){
 				newAverage = service.updateRate(1, rate-oldRate, id);
+				System.out.println("Ho aggiornato la media");
 				rateService.updateUserRate(name.getName(), id, rate);
+				System.out.println("Ho aggiornato il voto dello user");
 			}
 			else{
 				//aggiungo anche in tabelle di user-voto
